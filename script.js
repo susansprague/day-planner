f0f975bcc8dd554b1c58a75b3e7f5426864f529f
Your Name <you@example.com>
2023-05-03 15:57:10 -0400

    added changes to js to fit the description of the assignment

--- script.js
+++ script.js
@@ -2,6 +2,29 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+    var today = new Date()
+    var currentHour = today.getHours()
+    console.log(currentHour)
+    for(var i=9; i < 19; i++){
+        let colorKey = ""
+        if (i < currentHour){
+            colorKey= "past"
+        } else if (i === currentHour){
+            colorKey= "present"
+        } else {
+            colorKey= "future"
+        }
+        var timeBlock = $("<div>").addClass("row time-block past").attr("id", `hour-${i}`);
+        var hourDiv = $("<div>").addClass("col-2 col-md-1 hour text-center py-3").text(i);
+        var textArea = $("<textarea>").addClass("col-8 col-md-10 description "+ colorKey).attr("rows", "3").val(localStorage.getItem(`hour-${i}`))
+        var button = $("<button>").addClass("btn saveBtn col-2 col-md-1").attr("aria-label", "save").on("click", function(){
+        var hourKey = $(this).parent().attr("id")
+        var activity = $(this).siblings(".description").val()
+        localStorage.setItem(hourKey, activity)
+        })
+        var icon = $("<i>").addClass("fas fa-save").attr("aria-hidden", "true")
+        $(".container-fluid").append(timeBlock.append(hourDiv, textArea, button.append(icon)))
+    }
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
     // local storage. HINT: What does `this` reference in the click listener
